9302b273c890de446770d6dce40de4518489ed2a
valentino <valentinomino122@gmail.com>
2024-09-04 09:55:57 -0300

    primer commit

---
+++ cosas.ts
@@ -0,0 +1,23 @@
+interface Cosas {
+    id: number;
+    name: string;
+    description: string;
+  }
+  
+  // Creamos el módulo cosas
+  const cosas = {
+    // Creamos la colección de objetos
+    collection: [
+      { id: 1, name: "Objeto 1", description: "Descripción del objeto 1" },
+      { id: 2, name: "Objeto 2", description: "Descripción del objeto 2" },
+      { id: 3, name: "Objeto 3", description: "Descripción del objeto 3" },
+    ],
+    // Método para obtener la colección completa
+    getAll: function (): Cosas[] {
+      // Tu código
+    },
+     // Método para obtener un objeto por id
+     getById: function (id: number): Cosas | undefined {
+        // Tu código
+     },
+    };  
